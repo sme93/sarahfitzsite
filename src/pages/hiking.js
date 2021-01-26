201005c19585cb7a344cf54a5c8f50baf898d0c3
Sarah Fitzsimons <sarahfitz2713@gmail.com>
2021-01-26 13:00:44 -0700

    Add new hiking page image and CSS

--- src/pages/hiking.js
+++ src/pages/hiking.js
@@ -2,19 +2,23 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+import HikeImage from "../components/hikeImage"
 import SEO from "../components/seo"
 
 const HikingListPage = () => (
   <Layout>
     <SEO title="Hiking List Page" />
     <h1>hiking</h1>
-    <p>This is where I post about hikes.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="intro-box">
+      <p className="intro-p">Hiking in Rocky Mountain National Park and the surrounding areas.</p>
+    </div>  
+    <div>
+      <HikeImage />
     </div>
-    <Link to="/skypond/">Click here for  the hike to Sky Pond - January 2021</Link> <br />
-    <Link to="/frozenlake/">Click here for the hike to Frozen Lake - October 2020</Link> <br />
+    <div className="intro-box">
+      <Link to="/skypond/">Click here for  the hike to Sky Pond - January 2021</Link> <br />
+      <Link to="/frozenlake/">Click here for the hike to Frozen Lake - October 2020</Link> <br />
+    </div>
   </Layout>
 )
 
